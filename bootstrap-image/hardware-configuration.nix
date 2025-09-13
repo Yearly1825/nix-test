31c7403612c61ea1560a4544ff0acb76e6cc8440
josh <j@js-MacBook-Air.local>
2025-09-13 13:16:43 -0400

    Update hardware-configuration.nix

--- bootstrap-image/hardware-configuration.nix
+++ bootstrap-image/hardware-configuration.nix
@@ -1,44 +1,29 @@
-{ config, lib, pkgs, ... }:
-
+{ config, pkgs, ... }:
 {
-  # Import base Raspberry Pi configuration
-  imports = [
-    <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
-  ];
-
-  # Filesystem configuration
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXOS_SD";
+    fsType = "ext4";
+    options = [ "noatime" ];
   };
-
-  # Boot configuration specific to Pi
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/FIRMWARE";
+    fsType = "vfat";
+  };
   boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4;
-    # For Pi 3, use: pkgs.linuxPackages_rpi3
-    
-    # Enable additional hardware support
+    kernelPackages = pkgs.linuxPackages_rpi4; # For Pi 4; use linuxPackages_rpi5 for Pi 5
     loader.raspberryPi = {
       enable = true;
-      version = 4;  # Change to 3 for Pi 3
+      version = 4; # Change to 3 or 5 for other models
     };
+    loader.raspberryPi.firmwareConfig = ''
+      gpu_mem=256
+    '';
   };
-
-  # Hardware-specific options
   hardware = {
     enableRedistributableFirmware = true;
-    raspberry-pi."4".apply-overlays-dtmerge.enable = true;
     deviceTree = {
       enable = true;
-      filter = "*rpi-4-*.dtb";  # Change for different Pi models
+      filter = "*rpi-4-*.dtb"; # Adjust for Pi model
     };
   };
-
-  # GPU memory split (MB)
-  boot.loader.raspberryPi.firmwareConfig = ''
-    gpu_mem=256
-  '';
 }
