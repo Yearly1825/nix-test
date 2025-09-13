2ae33c9a7607af4a2dfd4ee18e491f4a04cb3fa2
josh <j@js-MacBook-Air.local>
2025-09-13 13:42:23 -0400

    Update hardware-configuration.nix

--- bootstrap-image/hardware-configuration.nix
+++ bootstrap-image/hardware-configuration.nix
@@ -9,13 +9,7 @@
     device = "/dev/disk/by-label/FIRMWARE";
     fsType = "vfat";
   };
-  boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4; # For Pi 4; use linuxPackages_rpi5 for Pi 5
-    # Optional: Firmware config for GPU memory (if still needed)
-    loader.firmwareConfig = ''
-      gpu_mem=256
-    '';
-  };
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
   hardware = {
     enableRedistributableFirmware = true;
     deviceTree = {
